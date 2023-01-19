bcdc1151f608b18607e30fe4d4d36ea3d2f91031
youramon <bay000r.man@gmail.com>
2023-01-19 22:09:22 +0100

    dasDings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'folder/search-engines',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'anfang',
+    loadChildren: () => import('./views/anfang/anfang.module').then( m => m.AnfangPageModule)
   }
+
 ];
 
 @NgModule({
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import *as db from "./db"
+import *as logik from "./logik"
+
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +11,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder!: string;
+  public counter = 0;
+  public randcolor: number[] = [254,50,2];
+  listentest(){
+    this.counter = this.counter +1;
+    logik.listSort()
+  }
 
+  public jzListe = ["a","b","c"];
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+   
+      this.jzListe = db.alleListen[this.folder];
+      for (var _i = 0; _i < this.randcolor.length; _i++){
+        this.randcolor[_i] = Math.round(Math.random()*255);
+      }
+    
+    
   }
 
 }
+
+//aaa
