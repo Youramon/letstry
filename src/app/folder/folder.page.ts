1a17e435a3d8b1cf2f6f716fa772aa4875edb6c0
youramon <bay000r.man@gmail.com>
2023-02-19 15:58:37 +0100

    mit Alört und bagfix

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
 import *as db from "./db"
 import *as logik from "./logik"
 import {DbbackupPage} from "src/app/views/dbbackup/dbbackup.page"
-import { SharedHeaderComponent } from '../components/shared-header/shared-header.component';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
@@ -16,7 +15,7 @@ import { SharedHeaderComponent } from '../components/shared-header/shared-header
 
 export class FolderPage implements OnInit {
  
-  
+  public wurdeAlörtgezeigt: boolean = false;
   public isOpen: boolean = false;
   public folder!: string;
   public counter = 0;
@@ -26,14 +25,24 @@ public randcolorray2 = [2,6,8]
   return Math.round(Math.random()*255);
 }
 
-  listentest(){
+  async listentest(){
     this.counter = this.counter + 1;
     logik.listSort()
+    if(this.jzListe.length == 3 && !this.wurdeAlörtgezeigt){
+    const alert = await this.alertController.create({
+          header: 'Achtung!',
+          subHeader: 'Länge der Liste wird kritisch uwu',
+          message: 'Recherchier schonmal nach etwas Neuem',
+          buttons: ['OK'],
+        });
+
+        await alert.present();
+        this.wurdeAlörtgezeigt = true;
+      }
   }
  public jzListe: logik.listMemba[] = [new logik.listMemba("aaa",45,"bbb")];
 
-  component = DbbackupPage;
-constructor(private activatedRoute: ActivatedRoute ,private router: Router) { }
+constructor(private activatedRoute: ActivatedRoute, private alertController: AlertController) { }
 
 
  
@@ -41,12 +50,14 @@ constructor(private activatedRoute: ActivatedRoute ,private router: Router) { }
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
    
       this.jzListe = db.alleListen[this.folder];
-      for(var i = 0; i < 3; i++){
+      for(var i = 0; i < 3; i++)
+      {
       this.randcolorray[i] = this.randcolor();
       this.randcolorray2[i] = this.randcolor();
       }
-    
-  }
+    this.wurdeAlörtgezeigt = false;
+  
+    }
 
 }
 
