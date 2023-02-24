b940f666815aa04f4fa1abdae1adf7f3a03d3f6d
youramon <bay000r.man@gmail.com>
2023-02-24 15:42:27 +0100

    jz Funktioniert es glaub pog

---
+++ src/app/otherAssets/constants.ts
@@ -0,0 +1,6 @@
+export const searchEnginesConst = "search-engines";
+export const chattingAppsConst = "chatting-apps";
+export const streamingUnlimitedConst = "streaming-unlimited";
+export const cloudsConst = "clouds";
+export const youtubeSocialsConst = "youtube-socials";
+export const videoEditorsConst = "video-editors";
